47402eab531addf6be4e0ac3150c93050f49d273
Andrew Fleenor <andrewflnr@gmail.com>
2013-12-23 19:11:54 -0800

    refactor reader forms to separate type, more test ouput

--- main.ml
+++ main.ml
@@ -1,20 +1,25 @@
-type patchSegment = KeepChars of int
-                  | DelChars of int
+type readerSegment = KeepChars of int
+                   | DelChars of int
+;;
+
+type patchSegment = Reader of readerSegment
                   | InsChars of string
+;;
+
+let str_of_readerSegment r =
+    match r with
+    | KeepChars n -> "{K" ^ string_of_int n ^ "}"
+    | DelChars n  -> "{D" ^ string_of_int n ^ "}"
 
 let str_of_patchSegment segment =
     match segment with
-     | KeepChars n ->
-        "{K" ^ string_of_int n ^ "}"
-     | DelChars n ->
-        "{D" ^ string_of_int n ^ "}"
-     | InsChars s ->
-        s ;;
+    | Reader r -> str_of_readerSegment r
+    | InsChars s -> s ;;
 
 let strdrop s n = String.sub s n ((String.length s) - n);;
 
-let advance segment dist = 
-    match segment with
+let advanceReader r dist=
+    match r with
         | KeepChars n ->
             if dist < n then
                 KeepChars (n - dist)
@@ -25,11 +30,22 @@ let advance segment dist =
                 DelChars (n - dist)
             else
                 failwith "advanced DelChars too far"
-        | InsChars s ->
-            try
-                InsChars (strdrop s dist)
-            with Invalid_argument msg -> failwith "advanced InsChars too far"
 ;;
 
-print_string ((str_of_patchSegment (advance (InsChars "the lazy dog jumped whatever") 5)) ^ "\n");;
+let advance segment dist = 
+    match segment with
+    | Reader r -> Reader (advanceReader r dist)
+    | InsChars s ->
+        try
+            InsChars (strdrop s dist)
+        with Invalid_argument msg -> failwith "advanced InsChars too far"
+;;
+
+let println s = (print_string s; print_string "\n");;
+
+println (str_of_patchSegment (Reader (KeepChars 34)));;
+println (str_of_patchSegment (Reader (DelChars 34)));;
+println (str_of_patchSegment (advance (InsChars "the lazy dog jumped whatever") 5));;
+println (str_of_patchSegment (advance (Reader (KeepChars 34)) 14));;
+println (str_of_patchSegment (advance (Reader (DelChars 34)) 14));;
 
