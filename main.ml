11266db329180ebbd19587cd0bafcec926073c37
Andrew Fleenor <andrewflnr@gmail.com>
2013-12-23 18:20:47 -0800

    Initial commit, str repr and advance fns for patch segments

---
+++ main.ml
@@ -0,0 +1,34 @@
+type patchSegment = KeepChars of int
+                  | DelChars of int
+                  | InsChars of string
+
+let str_of_patchSegment segment =
+    match segment with
+     | KeepChars n ->
+        "{K" ^ string_of_int n ^ "}"
+     | DelChars n ->
+        "{D" ^ string_of_int n ^ "}"
+     | InsChars s ->
+        s ;;
+
+let advance segment dist = 
+    match segment with
+        | KeepChars n ->
+            if dist < n then
+                KeepChars (n - dist)
+            else
+                failwith "advanced KeepChars too far"
+        | DelChars n ->
+            if dist < n then
+                DelChars (n - dist)
+            else
+                failwith "advanced DelChars too far"
+        | InsChars s ->
+            try
+                InsChars (String.sub s dist ((String.length s) - dist))
+            with Invalid_argument msg -> failwith "advanced InsChars too far"
+;;
+
+
+print_string ((str_of_patchSegment (advance (InsChars "the lazy dog jumped whatever") 5)) ^ "\n");;
+
